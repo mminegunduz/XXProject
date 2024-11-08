474b3225913ccdf2f490cc2828f432a860426fbe
Mine <142803161+mminegunduz@users.noreply.github.com>
2024-11-08 14:26:15 +0300

    Delete XX/XX directory

--- XX/XX/XXapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class XxappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'XXapp'
--- XX/XX/XXapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Tweet(models.Model):
-    kullaniciad= models.CharField(max_length=12)
-    mesaj=models.CharField(max_length=350)
-
-    def __str__(self):
-        return f"Tweet kullanici: {self.kullaniciad} mesaj:{self.mesaj}"
